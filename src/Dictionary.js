bfbc88c1f53bb4c9f1f9499ed21504b5f888fc94
NadiasCodes <De.nadia@outlook.com>
2024-02-13 11:54:52 +0100

    created a new state called loaded so i can have always something to show everytime we visit the page. Also made a lot of css changes

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,38 +3,57 @@ import axios from "axios";
 import Results from "./Results";
 import "./Dictionary.css";
 
-export default function Dictionary() {
-  let [keyword, setKeyword] = useState("");
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState (false);
+  
 
   function handleResponse(response) {
-    console.log(response.data);
     setResults(response.data);
   }
 
-  function search(event) {
+  function handleSubmit(event) {
     event.preventDefault();
-   
+    search();
+  }
+
+  function search() {
     const apiKey = `57t510363ca64d76cf8d437ao0eea1eb`;
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word= ${keyword}&key=${apiKey}`;
-
     axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
+  
   }
 
-  return (
-    <div className="Dictionary">
-      <form onSubmit={search}>
-        <input
-          type="search"
-          onChange={handleKeywordChange}
-          className="form"
-        ></input>
-      </form>
-      <Results  results={results}/>
-    </div>
-  );
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <section>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="search"
+              onChange={handleKeywordChange}
+              className="form"
+            ></input>
+          </form>
+          <div className="hint">
+            i.e. tea, yoga, nature, travel, coding, design
+          </div>
+        </section>
+        <Results results={results} />
+      </div>
+    );
+  } else {
+    load();
+    return "loading";
+  }
 }

aafe458a8245f006fa200cb9ad999705de09400c
NadiasCodes <De.nadia@outlook.com>
2024-02-13 09:56:33 +0100

    created a new component to display the synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,12 +1,14 @@
 import React from "react";
-
+import Synonyms from "./Synonyms"
 export default function Meaning(props) {
   console.log(props.meaning);
 
   return (
     <div className="Meaning">
-      <h3>{props.meaning.partOfSpeech}</h3><p>{props.meaning.definition}</p>
-      <p>{props.meaning.synonyms}</p>
+      <h3>{props.meaning.partOfSpeech}</h3>
+      <p> <strong>Definition:</strong> {props.meaning.definition}</p>
+      <p> <strong>Example:</strong> {props.meaning.example}</p>
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }
